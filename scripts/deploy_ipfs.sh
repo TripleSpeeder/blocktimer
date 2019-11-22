32faf67ad3e10e411a58a5e462260040ce00b7d8
Michael Bauer <michael@m-bauer.org>
2019-11-22 12:59:03 +0100

    Fix path to build path

--- scripts/deploy_ipfs.sh
+++ scripts/deploy_ipfs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Expected arguments: deploy_ipfs.sh <pinner service> <web3 connection string>
-ipfs-deploy build -p $1 -O \
+ipfs-deploy ../build -p $1 -O \
 | ens-updater setContenthash blocktimer.dappstar.eth \
 --web3 $2 \
 --contenttype ipfs-ns \
