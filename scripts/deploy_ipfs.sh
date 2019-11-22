1a0aed2d05046f7cd6976eb09b46986f7b1e5ab7
Michael Bauer <michael@m-bauer.org>
2019-11-22 12:47:07 +0100

    add script for ipfs-deploys

---
+++ scripts/deploy_ipfs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Expected arguments: deploy_ipfs.sh <pinner service> <web3 connection string>
+ipfs-deploy build -p $1 -O \
+| ens-updater setContenthash blocktimer.dappstar.eth \
+--web3 $2 \
+--contenttype ipfs-ns \
+--contenthash stdin
