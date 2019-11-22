e17b9cecd2489c793f39bb3caffcf03f99967f92
Michael Bauer <michael@m-bauer.org>
2019-11-22 15:43:39 +0100

    Fix deploy script

--- scripts/deploy_ipfs.sh
+++ scripts/deploy_ipfs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Expected arguments: deploy_ipfs.sh <pinner service> <web3 connection string>
-ipfs-deploy build -p $1 -C -O | ens-updater setContenthash blocktimer.dappstar.eth contenttype ipfs-ns contenthash stdin --web3 $2
+ipfs-deploy build -p $1 -C -O | ens-updater setContenthash blocktimer.dappstar.eth ipfs-ns stdin --web3 $2
